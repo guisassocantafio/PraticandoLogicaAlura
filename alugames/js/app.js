1e5c3773dcace8428e96810e6d5227ad36a15341
Guilherme Sasso Cantafio <gsassocantafio@gmail.com>
2025-06-09 17:30:38 -0300

    Contador de jogos alugados

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -1,28 +1,32 @@
-function alterarStatus(id){
+let jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+
+function exibirTotalAlugados() {
+    console.log(`Total de jogos alugados: ${jogosAlugados}`);
+}
+
+function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
     let nomeJogo = gameClicado.querySelector('.dashboard__item__name').textContent;
 
-    if (imagem.classList.contains('dashboard__item__img--rented')){
-        
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
         let confirmacao = window.confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo}?`);
 
-        if(confirmacao) {
-            console.log(`O Jogo ${nomeJogo} foi devolvido com sucesso.` );
+        if (confirmacao) {
             imagem.classList.remove('dashboard__item__img--rented');
-            botao.classList.remove('dashboard__item__button--return')
+            botao.classList.remove('dashboard__item__button--return');
             botao.textContent = 'Alugar';
-
-        } else {
-            console.log(`A devolução do jogo ${nomeJogo} foi cancelada.` );
+            jogosAlugados--;
         }
-
-    } 
-    else{
+    } else {
         imagem.classList.add('dashboard__item__img--rented');
-        botao.classList.add('dashboard__item__button--return')
+        botao.classList.add('dashboard__item__button--return');
         botao.textContent = 'Devolver';
-
+        jogosAlugados++;
     }
+
+    exibirTotalAlugados();
 }
+
+exibirTotalAlugados();
